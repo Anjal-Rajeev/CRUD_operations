9a4d01f8f6ca897460b866f6371b39da99e0c98e
Anjal-Rajeev <anjalrajeev5@gmail.com>
2023-03-10 01:31:25 +0530

    all APIs are done

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const logger = require('morgan');
 
-const app = new express()
+const app = new express()    // creating an instance of express
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -11,13 +11,13 @@ app.use(logger('dev'))
 
 
 require('dotenv').config()
-require('./middlewares/mongoDB')
+require('./middlewares/mongoDB')    // database connection
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000   
 
 
-const studentApi = require('./routes/studentApi')
+const studentApi = require('./routes/studentApi')  // api calls with "/student" in it will be redirect to studentApi 
 app.use('/student', studentApi) 
   
 
--- backend/middlewares/mongoDB.js
+++ backend/middlewares/mongoDB.js
@@ -3,11 +3,11 @@ mongoose.set('strictQuery', true)
 
 require('dotenv').config()
 
-const MONGO_URL = process.env.MONGO_URL
+const MONGO_URL = process.env.MONGO_URL    
 
-mongoose.connect(MONGO_URL)
+mongoose.connect(MONGO_URL)                // connecting database using connection string 
 .then(()=>{
-    console.log('---------mongoDB connected successfully---------');
+    console.log('---------mongoDB connected successfully---------');    
 })
 .catch((error)=>{
     console.log(error.message);
